ebe0056826284d913bc73f753797ba998164ea21
andrea-everett <andrea.dawn.everett@gmail.com>
2022-11-26 11:54:16 -0500

    pinterest-layout-working

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+import PinterestLayout from './components/PinterestLayout.js';
+
+
+function App() {
+  return (
+      <PinterestLayout />
+  );
+}
+
+export default App;
---
+++ src/components/PinterestLayout.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import Pin from "./Pin.js";
+
+
+ function PinterestLayout() {
+  return (
+
+                // <div pin_container='container grid-template-columns: repeat(auto-fill, 250px) bg-black'>
+                //             <div Pin='box-border h-260 w-250 m-15 bg-red'></div>
+                //             <div Pin='box-border h-330 w-250 bg-red'></div>
+                //             <div Pin='box-border h-450 w-250 bg-red'></div>
+                //             <div Pin='box-border h-260 w-250 m-15 bg-red'></div>
+                //             <div Pin='box-border h-330 w-250 bg-red'></div>
+                //             <div Pin='box-border h-450 w-250 bg-red'></div>
+                //             <div Pin='box-border h-260 w-250 m-15 '></div>
+                //             <div Pin='box-border h-330 w-250'></div>
+                //             <div Pin='box-border h-450 w-250'></div>
+                //     </div>
+
+                    <div style={styles.pin_container}>
+                            <Pin size='small' />
+                            <Pin size='medium' />
+                            <Pin size= 'large' />
+                            <Pin size='small' />
+                            <Pin size='medium' />
+                            <Pin size= 'large' />
+                            <Pin size='small' />
+                            <Pin size='medium' />
+                            <Pin size= 'large' />
+                    </div>
+  )
+}
+
+const styles = {
+  pin_container: {
+    margin: 0,
+    padding: 0,
+    width: '80vw',
+    backgroundColor: 'black',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, 250px)',
+    gridAutoRows: '10px',
+    justifyContent: 'center'
+  }
+}
+
+
+
+export default PinterestLayout;
